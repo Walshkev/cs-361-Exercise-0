ba3e8951210d55252a0fdef97f8fa517b71d8341
Kevin <walshkev@oregonstate.edu>
2023-09-27 12:15:58 -0700

    added coments

--- assignment1.rb
+++ assignment1.rb
@@ -1,9 +1,18 @@
 
+#kevin walsh 
+# cs 361 
+# assignment 1 
 
+# sets a variable to 1 
 x=1 
+# while the number is less than or equal to 1000 keep running 
 while x <= 1000
 
+    # sets new variable to the square root of x to not compromize the value of x 
     y= Math.sqrt(x)
+        #prints the value of y 
         puts y;
+    # incroments up by 1 
     x = x+1
+# ends the while loop
 end
